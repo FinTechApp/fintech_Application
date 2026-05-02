bdb73a9f943bf9599978be9c559cef6ef91620f9
Olatunji <olatee54@gmail.com>
2026-05-02 12:17:31 +0100

    new commit

--- fintech_app/src/pages/Transfer/TransferSuccess.jsx
+++ fintech_app/src/pages/Transfer/TransferSuccess.jsx
@@ -1,9 +1,23 @@
-const TransferSuccess = () => {
-  return (
-    <div style={{ padding: 40, fontFamily: 'DM Sans, sans-serif' }}>
-      <h1>TransferSuccess</h1>
-    </div>
-  )
-}
+import React from "react";
+import "./styles.css";
 
-export default TransferSuccess
+const TransferSuccess = ({ onDone }) => {
+  return (
+    <div className="containerr">
+      <div className="content">
+        <div className="icon success">✓</div>
+
+        <h1 className="title">Limit increased successfully</h1>
+        <p className="subtitle">
+          Your account limit has been increased!
+        </p>
+
+        <button className="done-btn" onClick={onDone}>
+          Done
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TransferSuccess;
