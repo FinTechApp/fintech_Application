a76be7e76dc727cca0bf3d8eab9e40e810d33cb1
Chhidera <chipeter633@gmail.com>
2026-06-11 10:13:05 +0100

    m

--- fintech_app/src/pages/Auth/SignUp.jsx
+++ fintech_app/src/pages/Auth/SignUp.jsx
@@ -8,7 +8,7 @@
 import { useState, useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../../context/AuthContext"
-const logo = "/logo.svg"
+const logo = "/logo2.png"
 
 const RED = "#E8402A"
 const FONT = "'DM Sans', sans-serif"
